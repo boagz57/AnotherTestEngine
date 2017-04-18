6f0f4528b61b879579c99109f81eb3f60a1762ae
Jason Bogert <boagz4@gmail.com>
2017-04-17 20:18:01 -0700

    Added SDL and SDL_Image libraries to project. Also edited post build scripts so that everything runs properly.

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,20 +1,9 @@
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_primitives.h>
+#include "SDL/SDL.h"
 #include "Window.h"
 
-int main()
+int main(int agrc, char** argv)
 {
-	al_init();
-	al_init_font_addon();
-	al_init_primitives_addon();
+	SDL_Init(SDL_INIT_EVERYTHING);
 
-	ALLEGRO_DISPLAY* display = al_create_display(800, 600);
-	ALLEGRO_FONT* font = al_create_builtin_font();
-	al_clear_to_color(al_map_rgb(0, 0, 0));
-	ALLEGRO_COLOR electricBlue = al_map_rgb(44, 177, 255);
-	al_draw_triangle(10.0f, 15.0f, 15.0f, 15.0f, 10.0f, 25.0f, electricBlue, 3.0f);
-	al_flip_display();
-	al_rest(5.0);
 	return 0;
 }
