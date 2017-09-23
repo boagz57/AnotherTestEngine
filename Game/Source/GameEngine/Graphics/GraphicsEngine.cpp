fca924337221300dd14b8e8c029e60a7f6df65dd
Jason Bogert <boagz4@gmail.com>
2017-09-22 22:44:12 -0400

    Modified graphics code to get animations playing again
    Added back some graphics code to get my updated uv coordinates sent down to gpu to have sprite animated.

--- Game/Source/GameEngine/Graphics/GraphicsEngine.cpp
+++ Game/Source/GameEngine/Graphics/GraphicsEngine.cpp
@@ -75,6 +75,8 @@ namespace Blz
 				glBindTexture(GL_TEXTURE_2D, fighter.GetSpriteSheet().GetTextureID());
 
 				glBindBuffer(GL_ARRAY_BUFFER, vboID);
+				glBufferData(GL_ARRAY_BUFFER, (sizeof(Vector3D) * fighter.GetSpriteSheet().GetVertexData().size()), &fighter.GetSpriteSheet().GetVertexData().front(), GL_DYNAMIC_DRAW);
+				glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vector3D), (void*)offsetof(Vector3D, textureCoordinates));
 
 				glDrawArrays(GL_TRIANGLES, 0, 6);
 
